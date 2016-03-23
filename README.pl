f4676586a0f7734c220214f8cdbfc579841c069b
Cheezmeister <@gmail>
2016-03-22 20:13:11 -0700

    FindSDL2.cmake

--- README.pl
+++ README.pl
@@ -1,6 +1,6 @@
 mkdir('cmake') or die 'trying';
 mkdir('cmake/Modules') or die 'trying';
-open(my $fh, '>', 'cmake/Modules/FindGLEW.cmake') or die "Could not open file '$filename' $!";
+open(my $fh, '>', 'cmake/Modules/FindGLEW.cmake') or die "Could not open file 'FindGLEW.cmake' $!";
 print $fh <<'HERE';
 find_path(GLEW_INCLUDE_DIR GL/glew.h)
 find_library(GLEW_LIBRARY NAMES GLEW glew32 glew glew32s PATH_SUFFIXES lib64)
@@ -17,4 +17,5 @@ endif()
 mark_as_advanced(GLEW_INCLUDE_DIR GLEW_LIBRARY)
 HERE
 close $fh;
-
+open(my $fh, '>', 'cmake/Modules/FindSDL2.cmake') or die "Could not open file 'FindSDL2.cmake' $!";
+close $fh;
